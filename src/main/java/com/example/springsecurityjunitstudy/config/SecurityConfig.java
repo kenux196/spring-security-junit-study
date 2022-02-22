5305c0b76096ee60767ef62d2f80d7db085b6639
kenux196 <kenux.yun@gmail.com>
2022-02-22 14:16:16 +0900

    test

--- src/main/java/com/example/springsecurityjunitstudy/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjunitstudy/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecurityjunitstudy.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
